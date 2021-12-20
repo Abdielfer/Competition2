d6d8dc0271c0bb4f820581fb0058abe231d079ab
Abdielfer <abdielfer@gmail.com>
2021-12-19 22:32:55 -0500

    transit 2

--- competition2.py
+++ competition2.py
@@ -52,30 +52,118 @@ def standardize_data(dataset, mean_std):
             dataset.iloc[n,i] /= mean_std[i][1]
     return dataset
 
-
-###Import Data
+                ## UNCOMMENT TO APPLY
+####    importing original datasets and filling NaN values 
 # train = pd.read_csv("train.csv", index_col = None)
 # y = train[['LABELS']]
+# ## x = pd.read_csv("x_standardized.csv", index_col = None) # to import sandardized dataset
 # x = train.drop('LABELS', axis=1)
+# ## Correction of empty values with means by columns
 # xMean = x.mean()
 # x = x.fillna(xMean)
-test_nolabels = pd.read_csv("test_nolabels.csv", index_col = None)
-test_nolabels_means = test_nolabels.mean()
-test_nolabels = test_nolabels.fillna(test_nolabels_means)
-toposElevation = {"topo_elevation_jan",'topo_elevation_feb','topo_elevation_mar','topo_elevation_apr','topo_elevation_may','topo_elevation_jun','topo_elevation_jul','topo_elevation_aug','topo_elevation_sep','topo_elevation_oct','topo_elevation_nov','topo_elevation_dec'}
-topoSlope = {'topo_slope_jan','topo_slope_feb','topo_slope_mar','topo_slope_apr','topo_slope_may','topo_slope_aug','topo_slope_jun','topo_slope_jul','topo_slope_sep','topo_slope_oct','topo_slope_nov','topo_slope_dec'}
-topoElevationDF = test_nolabels[toposElevation]
-topoElevationMean = topoElevationDF.mean(axis=1)
-test_nolabels = test_nolabels.drop(toposElevation,axis=1)
-test_nolabels['topoElevationMean'] = topoElevationMean
-topoSlopeDF = test_nolabels[topoSlope]
-topoSlopeMean = topoSlopeDF.mean(axis=1)
-test_nolabels = test_nolabels.drop(topoSlope, axis=1)
-test_nolabels['topoSlope'] = topoSlopeMean
-mean_std_x_train = meanStd(test_nolabels)
-test_nolabelsx = standardize_data(test_nolabels, mean_std_x_train)
-x.to_csv('nonLabeled_standardized.csv')
+# test_nolabels = pd.read_csv("test_nolabels.csv", index_col = None)
+# test_nolabels = test_nolabels.fillna(xMean)
+
+####     Replacing columns of elevation and slope by yearly means
+# toposElevation = {"topo_elevation_jan",'topo_elevation_feb','topo_elevation_mar','topo_elevation_apr','topo_elevation_may','topo_elevation_jun','topo_elevation_jul','topo_elevation_aug','topo_elevation_sep','topo_elevation_oct','topo_elevation_nov','topo_elevation_dec'}
+# topoSlope = {'topo_slope_jan','topo_slope_feb','topo_slope_mar','topo_slope_apr','topo_slope_may','topo_slope_aug','topo_slope_jun','topo_slope_jul','topo_slope_sep','topo_slope_oct','topo_slope_nov','topo_slope_dec'}
+# topoElevationDF = x[toposElevation]
+# topoElevationMean = topoElevationDF.mean(axis=1)
+# x = x.drop(toposElevation,axis=1)
+# x['topoElevationMean'] = topoElevationMean
+# topoSlopeDF = x[topoSlope]
+# topoSlopeMean = topoSlopeDF.mean(axis=1)
+# x = x.drop(topoSlope, axis=1)
+# x['topoSlope'] = topoSlopeMean
+
+####     preprocessing No_label datase:
+# test_nolabels = test_nolabels.fillna(xMean)
+# test_nolabels = test_nolabels.drop(toposElevation, axis=1)
+# test_nolabels = test_nolabels.drop(topoSlope, axis=1)
+# test_nolabels['topoElevationMean'] = topoElevationMean
+# test_nolabels['topoSlope'] = topoSlopeMean
+# ## standardizing test_nolabels
+# mean_std_x_noLabel = meanStd(test_nolabels)
+# test_nolabels = standardize_data(test_nolabels, mean_std_x_noLabel)
+
+## SPLITTING
+x_train, x_validation, y_train, y_validation = train_test_split( x,y, test_size=0.2)
+## Data shape exploration
+print("",np.shape(x_train),"  :",np.shape(x_validation) )
+print("Label balance on Training set: ", "\n", y_train['LABELS'].value_counts())
+print("Label balance on Validation set: ", "\n", y_validation['LABELS'].value_counts())
+## reshape classes in column vector
+y_train = (np.array(y_train).astype('int')).ravel()
+y_validation = (np.array(y_validation).astype('int')).ravel()
+
+### Some helpers function
+# To adapt the prediction to Kaggel format of submission 
+def formating_prediction(predictions): 
+        '''
+        Return the prediction in the Kaggle format submition
+        @hypotesis : column of classes 
+        '''
+        y_hat = pd.DataFrame({'S.No' : [],'LABELS' : []}, dtype=np.int8) 
+        for i in range(len(predictions)):
+            y_hat.loc[i] = [i,predictions[i]]
+        return pd.DataFrame(data = y_hat) 
+
+# Make predictions
+def predictOnSet(model, x_test):
+    prediction = model.predict(x_test)
+    return prediction
+
+def savingModels(classifier, modelFileName):
+    '''
+    NOTE: Do not forget the extention = *.pkl
+    Save as : 'modelFileName.pkl'
+    '''
+    joblib.dump(classifier, modelFileName)
 
 
+def importModel(modefname):
+    model = joblib.load(modefname)
+    return model
 
+def savePrediction(prediction, filename):
+    '''
+    Save predictions
+    @argument: filename: Remenber EXTENTION 'filename.csv'
+    '''
+    prediction = prediction.astype('int32') #exsure prediction as integer
+    predictions_DF = formating_prediction(prediction)
+    return predictions_DF.to_csv(filename, index = None)
 
+## modle evaluation
+def metric_RocAuc(y_probability, y_validation, estimator_name):
+    '''
+    Calculate and plt ROC metric
+    @argument: y_probability : the probability class=1.
+    @argument: y_validation: True labels.
+    fpr, tpr = false_positive, true_positive.
+    Return: "false_positive" and "true_positive", ROC_auc metric.
+    '''
+    fpr, tpr, _ = roc_curve(y_validation, y_probability) 
+    roc_auc = auc(fpr, tpr)
+    fig, axes = plt.subplots(constrained_layout=True,figsize=(5,3), dpi=150)
+    fig.suptitle(estimator_name)
+    axes.plot([0, 1], [0, 1], color= 'k',linestyle="--") # perfect fit 
+    display = metrics.RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc,
+                                       estimator_name=estimator_name)
+    display.plot(ax=axes)
+    return fpr, tpr, roc_auc
+
+## Show some evaluation criteria on the clasifier
+def evaluate_model(x_train, y_train, x_validation, y_validation, classifier):
+    features = x_train.columns
+    validation_Prediction = classifier.predict(x_validation)
+    validation_PredictedProb = classifier.predict_proba(x_validation)[:, 1]
+    ### ROC metric and curve #####
+    clasifierName = type(classifier).__name__
+    metric_RocAuc(validation_PredictedProb, y_validation,clasifierName)
+    fi_model = pd.DataFrame({'feature': features,
+                   'importance': classifier.feature_importances_}).\
+                    sort_values('importance', ascending = False)
+    clasifierNameExtended = clasifierName + "_info_fi.csv"     
+    fi_model.to_csv(clasifierNameExtended, index = None)
+    return fi_model
